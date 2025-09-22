c2659b2cbc73f3d550356f385a8ca134ae9a5f04
Nixarn <nixarn@gmail.com>
2025-09-22 17:50:30 +0300

    Replace DiscordProvider with GoogleProvider in auth config

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,6 +1,6 @@
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import type { DefaultSession, NextAuthConfig } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import GoogleProvider from "next-auth/providers/google";
 
 import { db } from "@/server/db";
 import {
@@ -38,11 +38,11 @@ declare module "next-auth" {
  */
 export const authConfig = {
 	providers: [
-		DiscordProvider,
+		GoogleProvider,
 		/**
 		 * ...add more providers here.
 		 *
-		 * Most other providers require a bit more work than the Discord provider. For example, the
+		 * Most other providers require a bit more work than the Google provider. For example, the
 		 * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
 		 * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
 		 *
